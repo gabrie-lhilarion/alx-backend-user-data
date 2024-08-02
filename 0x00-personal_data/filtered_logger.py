ae0d29bb351b1a3a49d47848ec33c2d9659da93e
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-08-02 20:53:22 +0100

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,9 +9,11 @@ specified fields with a redaction string.
 """
 
 import logging
+import os
 import re
+import mysql.connector
+from mysql.connector.connection import MySQLConnection
 from typing import List
-
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -80,3 +82,25 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
 
     return logger
+
+def get_db() -> MySQLConnection:
+    """
+    Obtain a connection to the secure Holberton database.
+
+    Uses environment variables to retrieve database credentials.
+
+    Returns:
+        MySQLConnection: A connector to the database.
+    """
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
+

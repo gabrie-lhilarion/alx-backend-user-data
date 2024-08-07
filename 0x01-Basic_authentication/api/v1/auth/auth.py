c5b398db0c780af428f3bfa7ed08f9af3ac59cc8
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-08-07 02:36:01 +0100

    which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,21 @@ class Auth:
         Method to determine if authentication is required.
         Currently returns False as a placeholder.
         """
-        return False
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        # Ensure path always ends with a slash for comparison
+        if not path.endswith('/'):
+            path += '/'
+
+        for ex_path in excluded_paths:
+            if ex_path.endswith('/') and ex_path == path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """

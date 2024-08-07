05853cacd40bfdb4055ce59ef87a3381d1480f00
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-08-07 03:46:58 +0100

    Basic - Base64 part correction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Class to manage basic authentication"""
+    
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
         Extracts the Base64 part of the Authorization header for Basic Authentication.

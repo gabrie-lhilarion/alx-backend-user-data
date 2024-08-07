07f3e06d1937afec7426bc4f131bc237b7aadd8e
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-08-07 01:01:37 +0100

    Error handler: forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,13 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ Forbidden endpoint
+    """
+    abort(403)
+
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ Unauthorized endpoint

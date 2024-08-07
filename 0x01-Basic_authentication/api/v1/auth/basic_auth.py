23f749790cfbad3845c69204d2c95da4c910a530
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-08-07 03:38:48 +0100

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module for API authentication management
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """Class to manage basic authentication"""
+    pass

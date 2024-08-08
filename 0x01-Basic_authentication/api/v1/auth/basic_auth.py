0aef95fb0c07eaeb184d54971371e59cacff5748
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-08-08 05:14:12 +0100

    Basic - User object corrected

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -115,8 +115,12 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        user = User.search(user_email)
-        if user is None or not user.is_valid_password(user_pwd):
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
             return None
 
         return user

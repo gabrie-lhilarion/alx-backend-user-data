c84f90b4a17de5e205671da9a72ea52cc363acfa
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-08-14 17:06:53 +0100

    Register user correction

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -41,4 +41,8 @@ class Auth:
             user = self._db.add_user(
                 email=email, hashed_password=hashed_password.decode('utf-8')
             )
-            return user
+            user_dict = {
+                "id": user.id,
+                "email": user.email,
+            }
+            return user_dict

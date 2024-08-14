26b99194692856915ebb0ebc065d4a837a411b43
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-08-14 06:41:31 +0100

    User model correction

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,8 +3,9 @@
 This module defines the SQLAlchemy model for the `users` table.
 
 The `User` model includes attributes such as `id`, `email`, `hashed_password`,
-`session_id`, and `reset_token`. These fields represent the primary key, 
-email address, hashed password, session ID, and password reset token, respectively.
+`session_id`, and `reset_token`. These fields represent the primary key,
+email address, hashed password, session ID, and password reset token,
+respectively.
 """
 
 from sqlalchemy import Column, Integer, String
@@ -16,7 +17,7 @@ Base = declarative_base()
 class User(Base):
     """
     Represents a user in the system.
-    
+
     Attributes:
         id (int): The primary key of the user.
         email (str): The email address of the user. Non-nullable.
@@ -25,7 +26,7 @@ class User(Base):
         reset_token (str): The reset token for password recovery. Nullable.
     """
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
